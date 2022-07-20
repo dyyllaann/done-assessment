4f9eea910d145831ed8aca36849d97a2df859f06
dyyllaann <dylan.arveson@gmail.com>
2022-07-20 11:54:44 -0700

    Rebuild app using Goxygen.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './App';
+import PatientForm from './components/PatientForm';
+import PatientDisplay from './components/PatientDisplay';
+import Success from './components/Success';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "@mui/material";
+
+ReactDOM.render(
+	<BrowserRouter>
+		<nav>
+			<Link href="/" underline="hover">
+				Home
+			</Link>
+			<Link href="/admin" underline="hover">
+				Admin Control Panel
+			</Link>
+		</nav>
+		<Routes>
+			<Route path="/" element={<PatientForm />} />
+			<Route path="/admin" element={<PatientDisplay />} />
+			<Route path="/success" element={<Success />} />
+		</Routes>
+	</BrowserRouter>,
+	document.getElementById("root")
+);
